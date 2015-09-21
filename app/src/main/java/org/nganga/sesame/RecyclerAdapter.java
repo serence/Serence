0fe83d7bb2640b95bdc6ca4b6a205b5c50d0f70a
nganga <mtnjoroge@gmail.com>
2015-09-21 04:17:05 +0300

    Created Adapter to associate data to views

--- app/src/main/java/org/nganga/sesame/RecyclerAdapter.java
+++ app/src/main/java/org/nganga/sesame/RecyclerAdapter.java
@@ -1,24 +1,34 @@
 package org.nganga.sesame;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * Created by nganga on 9/21/15.
  */
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, int position) {
 
     }
+
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+
+    class  MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
