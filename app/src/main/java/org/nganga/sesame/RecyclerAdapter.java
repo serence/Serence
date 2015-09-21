aa471fdb6e692fc04498db5e5ec067bf7b9ec6b9
nganga <mtnjoroge@gmail.com>
2015-09-21 05:28:50 +0300

    Added Viewholder to reuse single view

--- app/src/main/java/org/nganga/sesame/RecyclerAdapter.java
+++ app/src/main/java/org/nganga/sesame/RecyclerAdapter.java
@@ -1,16 +1,31 @@
 package org.nganga.sesame;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by nganga on 9/21/15.
  */
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+
+    public RecyclerAdapter(Context context){
+
+        inflater = LayoutInflater.from(context);
+    }
+
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view = inflater.inflate(R.layout.custom_row, parent, false);  //Inflate view elements from root custom xml
+        MyViewHolder holder = new MyViewHolder(view);   //custom viewholder used to reuse view components.
+        return holder;
     }
 
     @Override
@@ -27,8 +42,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     class  MyViewHolder extends RecyclerView.ViewHolder {
 
+        TextView title;
+        ImageView icon;
+
         public MyViewHolder(View itemView) {
             super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.listText);
+            icon = (ImageView) itemView.findViewById(R.id.listIcon);
         }
     }
 }
